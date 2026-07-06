d01e5f48b40da51c656848be9fabc61bc0eca163
omochimetaru <omochi.metaru@gmail.com>
2026-07-06 13:12:02 +0900

    remove woff1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import fontkit from './base';
 import TTFFont from './TTFFont';
-import WOFFFont from './WOFFFont';
 import TrueTypeCollection from './TrueTypeCollection';
 import DFont from './DFont';
 
 // Register font formats
 fontkit.registerFormat(TTFFont);
-fontkit.registerFormat(WOFFFont);
 fontkit.registerFormat(TrueTypeCollection);
 fontkit.registerFormat(DFont);
 
